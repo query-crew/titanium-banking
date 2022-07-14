ffd440a6c2a4ebef5bd1033ac56921437ab7d4df
Alex Monson <monson.alex@rocketmail.com>
2022-07-13 21:42:05 -0700

    added crud for accounts

---
+++ account/src/main/java/com/smoothstack/titaniumbanking/models/AccountType.java
@@ -0,0 +1,33 @@
+// package com.smoothstack.titaniumbanking.models;
+
+// import javax.persistence.*;
+
+// import lombok.*;
+
+// @Entity
+// @Getter
+// @Setter
+// @NoArgsConstructor
+// @ToString
+// @Table(name="Accounts_Types")
+// public class AccountType {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     @Column(name="account_type_id")
+//     private int accountTypeId;
+
+//     @Column(name="savings")
+//     private boolean savings;
+
+//     @Column(name="investing")
+//     private boolean investing;
+
+//     @OneToOne(mappedBy = "accountTypeId")
+//     private Account account;
+
+//     public AccountType(boolean savings, boolean investing){
+//         super();
+//         this.savings = savings;
+//         this.investing = investing;
+//     }
+// }
---
+++ account/src/test/java/com/smoothstack/titaniumbanking/TitaniumBankingApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.smoothstack.titaniumbanking;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class TitaniumBankingApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
