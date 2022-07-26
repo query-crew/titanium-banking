2b3ca6a32d1280c85d5c65613fcb075c51f2ff3b
alexdongss <alexander.dong@smoothstack.com>
2022-07-26 11:49:12 -0700

    added routing to transactions

--- titanium-admin/src/app/app-routing.module.ts
+++ titanium-admin/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { AddBranchPageComponent } from './modules/branch/pages/add-branch-page/a
 import { BranchListPageComponent } from './modules/branch/pages/branch-list-page/branch-list-page.component';
 import { BranchPageComponent } from './modules/branch/pages/branch-page/branch-page.component';
 import { EditBranchPageComponent } from './modules/branch/pages/edit-branch-page/edit-branch-page.component';
+import { TransactionPageComponent } from './modules/transaction-page/pages/transaction-page.component';
 
 //{ path: '', redirectTo: '/login', pathMatch: 'full' },
 //may need to change this when auth is implemented.
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'branch/add', component: AddBranchPageComponent },
   { path: 'branch/view/:id', component: BranchPageComponent },
   { path: 'branch/edit/:id', component: EditBranchPageComponent },
+  { path: 'transactions', component: TransactionPageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'home', component: MenuPageComponent },
   { path: '**', component: PageNotFoundPageComponent },
