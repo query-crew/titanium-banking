6b925ed5c1be0638e1be06f09d83346625bc65b5
chloejohnsonsmoothstack <chloe.johnson@smoothstack.com>
2022-07-27 10:39:52 -0600

    fixed bootstrap import

--- customer-portal/src/atoms/BankNavBar.js
+++ customer-portal/src/atoms/BankNavBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import "../bootstrap.css";
 import "./NavBar.css";
 import { useMediaQuery } from 'react-responsive';
 
--- customer-portal/src/index.js
+++ customer-portal/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { store } from './store'
 import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
