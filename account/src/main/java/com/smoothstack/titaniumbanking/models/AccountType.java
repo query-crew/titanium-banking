ffbff3124e67153ba7560f31efabc5cdb9e105b0
Alex Monson <monson.alex@rocketmail.com>
2022-07-27 13:05:11 -0700

    working on register account

--- account/src/main/java/com/smoothstack/titaniumbanking/models/AccountType.java
+++ account/src/main/java/com/smoothstack/titaniumbanking/models/AccountType.java
@@ -1,33 +1,33 @@
-// package com.smoothstack.titaniumbanking.models;
+package com.smoothstack.titaniumbanking.models;
 
-// import javax.persistence.*;
+import javax.persistence.*;
 
-// import lombok.*;
+import lombok.*;
 
-// @Entity
-// @Getter
-// @Setter
-// @NoArgsConstructor
-// @ToString
-// @Table(name="Accounts_Types")
-// public class AccountType {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     @Column(name="account_type_id")
-//     private int accountTypeId;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name="Accounts_Types")
+public class AccountType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="account_type_id")
+    private int accountTypeId;
 
-//     @Column(name="savings")
-//     private boolean savings;
+    @Column(name="savings")
+    private boolean savings;
 
-//     @Column(name="investing")
-//     private boolean investing;
+    @Column(name="investing")
+    private boolean investing;
 
-//     @OneToOne(mappedBy = "accountTypeId")
-//     private Account account;
+    @OneToOne(mappedBy = "accountTypeId")
+    private Account account;
 
-//     public AccountType(boolean savings, boolean investing){
-//         super();
-//         this.savings = savings;
-//         this.investing = investing;
-//     }
-// }
+    public AccountType(boolean savings, boolean investing){
+        super();
+        this.savings = savings;
+        this.investing = investing;
+    }
+}
