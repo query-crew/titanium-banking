89d07f17a209a85b201027c024ff1aa3d53af358
Alex Monson <monson.alex@rocketmail.com>
2022-08-09 14:38:41 -0700

    working on register account frontend page

--- customer-portal/src/RegisterAccount.js
+++ customer-portal/src/RegisterAccount.js
@@ -11,17 +11,17 @@ const RegisterAccount = () => {
     
     const [accountType, setAccountType] = useState('Account');
     const [accountName,  setAccountName] = useState('');
-    // const [firstName, setFirstName] = useState('');
-    // const [lastName, setLastName] = useState('')
-    // const [phoneNumber, setPhoneNumber] = useState('')
-    // const [dateOfBirth, setDateOfBirth] = useState('')
-    // const [ssn, setSsn] = useState('')
-    // const [addressOne, setAddressOne] = useState('')
-    // const [addressTwo, setAddressTwo] = useState('')
-    // const [city, setCity] = useState('')
-    // const [state, setState] = useState('')
-    // const [zipcode, setZipcode] = useState('')
-    // const [country, setCountry] = useState('')
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('')
+    const [phoneNumber, setPhoneNumber] = useState('')
+    const [dateOfBirth, setDateOfBirth] = useState('')
+    const [ssn, setSsn] = useState('')
+    const [addressOne, setAddressOne] = useState('')
+    const [addressTwo, setAddressTwo] = useState('')
+    const [city, setCity] = useState('')
+    const [state, setState] = useState('')
+    const [zipcode, setZipcode] = useState('')
+    
     const [validationErrors, setValidationErrors] = useState([])
 
     console.log("&*&(*&(&(*&*(&(*&(*", accountType);
@@ -33,16 +33,16 @@ const handleSubmit = async(e) => {
         const errors = [];
 
         if(accountType === "Account") errors.push("Please select an account type")
-        // if(!firstName.trim().length) errors.push("Please enter your first name")
-        // if(!lastName.trim().length) errors.push("Please enter your last name")
-        // if(!phoneNumber.trim().length) errors.push("Please enter your phone number")
-        // if(!dateOfBirth.trim().length) errors.push("Please enter your date of birth")
-        // if(!ssn.trim().length) errors.push("Please enter your social security number")
-        // if(!addressOne.trim().length) errors.push("Please enter your address")
-        // if(!city.trim().length) errors.push("Please enter a city")
-        // if(!state.trim().length) errors.push("Please enter a state")
-        // if(!zipcode.trim().length) errors.push("Please enter your zipcode")
-        // if(!country.trim().length) errors.push("Please enter a country")
+        if(!firstName.trim().length) errors.push("Please enter your first name")
+        if(!lastName.trim().length) errors.push("Please enter your last name")
+        if(!phoneNumber.trim().length) errors.push("Please enter your phone number")
+        if(!dateOfBirth.trim().length) errors.push("Please enter your date of birth")
+        if(!ssn.trim().length) errors.push("Please enter your social security number")
+        if(!addressOne.trim().length) errors.push("Please enter your address")
+        if(!city.trim().length) errors.push("Please enter a city")
+        if(!state.trim().length) errors.push("Please enter a state")
+        if(!zipcode.trim().length) errors.push("Please enter your zipcode")
+        
         
 
         if (errors.length){
@@ -52,22 +52,25 @@ const handleSubmit = async(e) => {
             // const config = { headers: {
             //     'Content-Type': 'multipart/form-data'
             // }}
-            // formData.append("accountType", accountType);
-            // formData.append("accountName", accountName);
-            // formData.append("firstName", firstName);
-            // formData.append("lastName", lastName);
-            // formData.append("phoneNumber", phoneNumber);
-            // formData.append("dateOfBirth", dateOfBirth);
-            // formData.append("ssn", ssn);
-            // formData.append("addressOne", addressOne);
-            // formData.append("addressTwo", addressTwo);
-            // formData.append("city", city);
-            // formData.append("state", state);
-            // formData.append("zipcode", zipcode);
-            // formData.append("country", country);
+
+            
+            const form = {
+                "accountType": accountType,
+                "accountName": accountName,
+                // "firstName": firstName,
+                // "lastName": lastName,
+                // "phoneNumber": phoneNumber,
+                // "dateOfBirth": dateOfBirth,
+                // "ssn": ssn,
+                // "addressOne": addressOne,
+                // "addressTwo": addressTwo,
+                // "city": city,
+                // "state": state,
+                // "zipcode": zipcode,
+            }
 
            
-                await axios.post(`${url}`, {"accountType": accountType, "accountName": accountName})
+                await axios.post(`${url}`, form)
                         .then((response) => {
                         console.log(response.data);
                         })
@@ -90,11 +93,9 @@ const handleSubmit = async(e) => {
    return(
        <section className='container border border-danger register-account-section'>
             <div className='row'>
-            
-                <h1 className='register-account-header bg-light rounded-top'>
+                <span className='register-account-header fs-1 mt-4 bg-light rounded-top'>
                     Register for Account
-                </h1> 
-                
+                </span>  
             <div className='form-errors'>
             {validationErrors.map((error, i) => (
                 <div key={i}>{error}</div>
@@ -102,18 +103,18 @@ const handleSubmit = async(e) => {
             </div>
            <form className='register-account-form bg-white'
                  onSubmit={handleSubmit}>
-                <h4>Account Type</h4>
-                <select className="col-9 form-select w-25" aria-label="Default select example" onChange={e => setAccountType(e.target.value)}>
+                <h6 className="mt-3 mx-3">Account Type</h6>
+                <select className="account-type-dropdown col-9 form-select mx-3" aria-label="Default select example" onChange={e => setAccountType(e.target.value)}>
                     <option defaultValue="Account">Account</option>
                     <option value="checking">Checking</option>
                     <option value="savings">Savings</option>
                     <option value="investing">Investing</option>
                     <option value="loan">Loan</option>
                 </select>
-                <label> Account Name</label>
-                <input className="col-4" value={accountName} onChange={e => setAccountName(e.target.value)}></input>
-                {/* <h3> Confirm Personal Information</h3>
-                    <label> Name </label>
+                <label> Account Name </label>
+                <input className="account-name col-4 mt-3" value={accountName} onChange={e => setAccountName(e.target.value)}></input>
+                <h5 className="mt-5"> Confirm Personal Information</h5>
+                    <label className="mx-3"> Name </label>
                 <div className="col-4">
                     <input className="p-1 " name='firstName' placeholder='First Name'  type='text' required={true} value={firstName} onChange={ e => setFirstName(e.target.value)}/>
                     <input className="p-1" name='lastName' placeholder='Last Name'  type='text' required={true} value={lastName} onChange={ e => setLastName(e.target.value)}/>
@@ -127,21 +128,110 @@ const handleSubmit = async(e) => {
                     <input className="p-1" name='ssn' placeholder='###-##-####'  type='text' required={true} value={ssn} onChange={ e => setSsn(e.target.value)}/>
                     </label>
                 </div>
-                <h3> Confirm Address</h3>
-                <div className="col-6">
-                    <input className="p-1" name='addressOne' placeholder='Street Address'  type='text' required={true} value={addressOne} onChange={ e => setAddressOne(e.target.value)}/>
-                    <input className="p-1" name='addressTwo' placeholder='Street Address 2'  type='text' required={true} value={addressTwo} onChange={ e => setAddressTwo(e.target.value)}/>
-                    <input className="p-1" name='city' placeholder='City'  type='text' required value={city} onChange={ e => setCity(e.target.value)}/>
-                    <input className="p-1" name='state' placeholder='State'  type='text' required value={state} onChange={ e => setState(e.target.value)}/>
-                    <input className="p-1" name='zipcode' placeholder='Zipcode'  type='text' required value={zipcode} onChange={ e => setZipcode(e.target.value)}/>
-                    <input className="p-1" name='country' placeholder='Country'  type='text' required value={country} onChange={ e => setCountry(e.target.value)}/>
-                </div> */}
+                <h5> Confirm Address</h5>
+                <div class="col-12">
+                    <label for="inputAddress" class="form-label">Address</label>
+                    <input type="text" class="form-control" id="inputAddress" placeholder="Street Address" />
+                </div>
+                <div class="col-12">
+                    <label for="inputAddress2" class="form-label">Address 2</label>
+                    <input type="text" class="form-control" id="inputAddress2" placeholder="Street Address 2" />
+                </div>
+                <div class="col-md-6">
+                    <label for="inputCity" class="form-label">City</label>
+                    <input type="text" class="form-control" id="city" />
+                </div>
+                <div class="col-md-4">
+                    <label for="inputState" class="form-label">State</label>
+                    <select id="state" class="form-select">
+                    <option selected>Choose...</option>
+                    <option value="AL">...</option>
+                    <option value="AK">...</option>
+                    <option value="AZ">...</option>
+                    <option value="AR">...</option>
+                    <option value="AS">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                    <option value="">...</option>
+                     
+                    </select>
+                </div>
+                <div class="col-md-2">
+                    <label for="inputZip" class="form-label">Zip</label>
+                    <input type="text" class="form-control" id="inputZip" />
+                </div>
                 <div className='register-account-btns mt-3 border-top register-form-btns bg-light rounded'> 
                     <button className='go-back  text-white btn btn-primary btn-lg ' type='click' onClick={handleCancel}>Go Back</button>
                     <button className='register-account-submit float-end text-white btn btn-primary btn-lg ' type="submit">Register</button>
                 </div>
            </form>
-
             </div>
        </section>
     );
@@ -149,3 +239,4 @@ const handleSubmit = async(e) => {
 }
 
 export default RegisterAccount;
+
