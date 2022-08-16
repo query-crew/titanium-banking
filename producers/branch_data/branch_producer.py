227ae40d0998d74df01c85b236ec2b927ca9d581
Wyatt Horton <hortonwhy@protonmail.com>
2022-08-16 17:20:28 -0500

    setup planning and skeleton of branch producer

---
+++ producers/branch_data/branch_producer.py
@@ -0,0 +1,34 @@
+""" Branch data producer that creates dummy branch data """
+
+
+class BranchProducer:
+    """
+    """
+
+    def __init__(self, n_records):
+        self.n_records = n_records
+
+        self.records_produced = []
+
+    def __repr__(self):
+        return f"Branch Producer to generate {self.n_records} records"
+
+    def verify_records(self):
+        pass
+
+    def branch_generator(self):
+        pass
+
+    def records_insert(self):
+        pass
+
+    def records_delete(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
