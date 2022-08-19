5b582269e832cd4c68d0fc2210afa2586e6b3e0e
chloejohnsonsmoothstack <106343473+chloejohnsonsmoothstack@users.noreply.github.com>
2022-08-19 12:23:35 -0600

    Delete SocialSecurityNumberNotFoundException.java

--- user/src/main/java/com/titanium/user/exception/SocialSecurityNumberNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.titanium.user.exception;
-
-public class SocialSecurityNumberNotFoundException extends BadRequestException {
-    public SocialSecurityNumberNotFoundException() {
-        super("Social security number could not be confirmed.");
-    }
-}
