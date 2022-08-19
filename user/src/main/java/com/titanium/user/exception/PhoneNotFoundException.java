654a2f8adfc8d7956aa1ddc321dd4178a618e6a5
chloejohnsonsmoothstack <106343473+chloejohnsonsmoothstack@users.noreply.github.com>
2022-08-19 12:23:21 -0600

    Delete PhoneNotFoundException.java

--- user/src/main/java/com/titanium/user/exception/PhoneNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.titanium.user.exception;
-
-public class PhoneNotFoundException extends BadRequestException {
-    public PhoneNotFoundException() {
-        super("Phone could not be confirmed.");
-    }
-}
