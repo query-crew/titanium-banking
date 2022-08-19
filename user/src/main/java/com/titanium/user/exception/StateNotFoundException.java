a7af72e9d94f2d30e715633945a486d5330bea25
chloejohnsonsmoothstack <106343473+chloejohnsonsmoothstack@users.noreply.github.com>
2022-08-19 12:23:49 -0600

    Delete StateNotFoundException.java

--- user/src/main/java/com/titanium/user/exception/StateNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.titanium.user.exception;
-
-public class StateNotFoundException extends BadRequestException {
-    public StateNotFoundException() {
-        super("State could not be confirmed.");
-    }
-}
