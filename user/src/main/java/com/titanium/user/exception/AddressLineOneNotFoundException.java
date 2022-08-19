d8d9a0b7cbe7bcb3106406f321ef185cd42bae53
chloejohnsonsmoothstack <106343473+chloejohnsonsmoothstack@users.noreply.github.com>
2022-08-19 12:21:19 -0600

    Delete AddressLineOneNotFoundException.java

--- user/src/main/java/com/titanium/user/exception/AddressLineOneNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.titanium.user.exception;
-
-public class AddressLineOneNotFoundException extends BadRequestException {
-    public AddressLineOneNotFoundException() {
-        super("Address line 1 could not be confirmed.");
-    }
-}
