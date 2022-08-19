dcf6eb4a2292f935a7ca3a9f24a11f95285375a8
chloejohnsonsmoothstack <106343473+chloejohnsonsmoothstack@users.noreply.github.com>
2022-08-19 12:23:06 -0600

    Delete LastNameNotFoundException.java

--- user/src/main/java/com/titanium/user/exception/LastNameNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.titanium.user.exception;
-
-public class LastNameNotFoundException extends BadRequestException {
-    public LastNameNotFoundException() {
-        super("Last name could not be confirmed.");
-    }
-}
