9a90f851767fc2fa9c78ac8c8c5c4b37e6b0064f
chloejohnsonsmoothstack <106343473+chloejohnsonsmoothstack@users.noreply.github.com>
2022-08-19 12:22:52 -0600

    Delete FirstNameNotFoundException.java

--- user/src/main/java/com/titanium/user/exception/FirstNameNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.titanium.user.exception;
-
-public class FirstNameNotFoundException extends BadRequestException {
-    public FirstNameNotFoundException() {
-        super("First name could not be confirmed.");
-    }
-}
