c55f31a111d55d00d8961ee6b6d4770e4be299ad
chloejohnsonsmoothstack <106343473+chloejohnsonsmoothstack@users.noreply.github.com>
2022-08-19 12:24:03 -0600

    Delete ZipcodeNotFoundException.java

--- user/src/main/java/com/titanium/user/exception/ZipcodeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.titanium.user.exception;
-
-public class ZipcodeNotFoundException extends BadRequestException {
-    public ZipcodeNotFoundException() {
-        super("Zipcode could not be confirmed.");
-    }
-}
