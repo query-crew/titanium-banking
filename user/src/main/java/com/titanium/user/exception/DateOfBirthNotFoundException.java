84c309871bda5b595ce0e7a3f402405dab540e07
chloejohnsonsmoothstack <106343473+chloejohnsonsmoothstack@users.noreply.github.com>
2022-08-19 12:22:36 -0600

    Delete DateOfBirthNotFoundException.java

--- user/src/main/java/com/titanium/user/exception/DateOfBirthNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.titanium.user.exception;
-
-public class DateOfBirthNotFoundException extends BadRequestException {
-    public DateOfBirthNotFoundException() {
-        super("Date of birth could not be confirmed.");
-    }
-}
